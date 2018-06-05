1170409e6d33305b160d2ba44c20112b314b7050
EC2 Default User <ec2-user@ip-172-31-42-128.us-east-2.compute.internal>
2018-06-05 04:42:48 +0000

    post

--- microposts/resources/views/commons/navbar.blade.php
+++ microposts/resources/views/commons/navbar.blade.php
@@ -13,7 +13,6 @@
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
                       @if (Auth::check())
-                        <li>{!! link_to_route('users.index', 'Users') !!}</li>
                         <li><a href="#">Users</a></li>
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
@@ -21,6 +20,7 @@
                                 <li><a href="#">My profile</a></li>
                                 <li role="separator" class="divider"></li>
                                 <li>{!! link_to_route('logout.get', 'Logout') !!}</li>
+                                <li>{!! link_to_route('users.index', 'Users') !!}</li>
                             </ul>
                         </li>
                     @else
